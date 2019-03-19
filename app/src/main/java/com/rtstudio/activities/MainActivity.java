f2c56fde6d90d10555f9c36281b378a9461f184a
Raphael Rodrigues de Oliveira <rodrigues.rph@gmail.com>
2019-03-19 15:22:19 -0300

    Alterado o nome da Main2Activity para SegundaActivity e activity_main2.xml para activity_segunda.xml

--- app/src/main/java/com/rtstudio/activities/MainActivity.java
+++ app/src/main/java/com/rtstudio/activities/MainActivity.java
@@ -18,7 +18,7 @@ public class MainActivity extends Activity {
         btnSegundaAct.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(MainActivity.this, Main2Activity.class));
+                startActivity(new Intent(MainActivity.this, SegundaActivity.class));
             }
         });
     }
