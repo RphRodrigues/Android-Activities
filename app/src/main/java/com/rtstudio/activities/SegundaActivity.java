b003ddc2e4a5f95c9bf68bdd40854a4acae13faf
Raphael Rodrigues de Oliveira <rodrigues.rph@gmail.com>
2019-03-19 14:24:46 -0300

    Segunda activity adicionada

---
+++ app/src/main/java/com/rtstudio/activities/SegundaActivity.java
@@ -0,0 +1,12 @@
+package com.rtstudio.activities;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class SegundaActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+}
